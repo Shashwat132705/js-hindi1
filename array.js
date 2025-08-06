378937bd96201c6c54dd4c7fa9634e56cac8ade7
Shashwat132705 <shashwatpandey0027@gmail.com>
2025-08-06 13:47:19 +0000

    array1

---
+++ array.js
@@ -0,0 +1,27 @@
+//array
+const myArr=[1,2,3,4,5,6,true,"hitesh"]
+const myHeros=["shaktimaan","naagraaj"]
+const myArr2=new Array(1,2,3,4)
+//console.log(myArr[1]);
+//++++++++++++++++++Array Methods++++++++++++++++++
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// 
+// myArr.unshift(333)
+
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(6));
+// console.log(myArr.indexOf(6));
+// console.log(myArr);
+const newArr=myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+// slice and splice
+console.log("A",myArr);
+const mynn1 = myArr.slice(1,3)
+console.log(mynn1);
+console.log("b",myArr);
+
